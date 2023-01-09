af51bdedd9e9f869fa4222bfd3a2dec5fba548eb
manvita-k <76105276+manvita-k@users.noreply.github.com>
2023-01-09 23:51:45 +0530

    comment

--- migrations/20221120052016-create-user.js
+++ migrations/20221120052016-create-user.js
@@ -1,5 +1,7 @@
 "use strict";
 /** @type {import('sequelize-cli').Migration} */
+
+//migrations--table
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("Users", {
