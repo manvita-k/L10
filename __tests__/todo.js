e66e93bb82b8075e61dbb7a7b04a69fd11c88d99
manvita-k <76105276+manvita-k@users.noreply.github.com>
2023-01-09 23:45:00 +0530

    comments

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-undef */
+
 const request = require("supertest");
 var cheerio = require("cheerio");
 const db = require("../models/index");
 const app = require("../app");
-//const todo = require("../models/todo");
+//const todo = models/todo
+
 let server, agent;
 
 function extractCsrfToken(res) {
